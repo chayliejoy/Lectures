f5edff3560fad83870ed902e466ca1987a53fa68
Hans-Joerg Schurr <commits@schurr.at>
2024-11-05 15:03:33 -0600

    Add rest of lecture 7

--- Lecture7/Lecture7.hs
+++ Lecture7/Lecture7.hs
@@ -81,3 +81,104 @@ asPair :: Expr -> (Expr, Expr)
 asPair (Pair m n) = (m, n)
 asPair e = error ("Expected " ++ show e ++ " to be a pair constant")
 
+{-------------------------------------------------------------------------------
+
+Two claims about this function.
+
+First: if `bigStep m` results in `n`, then `isValue n`.  
+
+
+-------------------------------------------------------------------------------}
+
+
+
+
+-- Claim: bigStem m == m'  ==>  isValue m'
+
+data Value = VInt Int | VPair Value Value
+  deriving Show
+
+intv :: Value -> Int
+intv (VInt i) = i
+intv _ = error "int expected"
+
+pairv :: Value -> (Value, Value)
+pairv (VPair v w) = (v, w)
+pairv _ = error "pair expected"
+
+bigStep' :: Expr -> Value
+bigStep' (Const i) = VInt i
+bigStep' (Plus m n) = VInt (intv (bigStep' m) + intv (bigStep' n))
+bigStep' (Times m n) = VInt (intv (bigStep' m) * intv (bigStep' n))
+bigStep' (Negate m) = VInt (negate (intv (bigStep' m)))
+bigStep' (Pair m n) = VPair (bigStep' m) (bigStep' n)
+bigStep' (Fst m) = fst (pairv (bigStep' m))
+bigStep' (Snd m) = snd (pairv (bigStep' m))
+
+smallStep :: Expr -> Expr
+smallStep (Const i) = error "no step"
+smallStep (Plus (Const i) (Const j)) = Const (i + j)
+smallStep (Plus m n)
+  | isValue m = Plus m (smallStep n)
+  | otherwise = Plus (smallStep m) n
+smallStep (Times (Const i) (Const j)) = Const (i * j)
+smallStep (Times m n)
+  | isValue m = Times m (smallStep n)
+  | otherwise = Times (smallStep m) n
+smallStep (Negate (Const i)) = Const (negate i)
+smallStep (Negate m) = Negate (smallStep m)
+smallStep (Pair m n)
+  | isValue m = Pair m (smallStep n)
+  | otherwise = Pair (smallStep m) n
+smallStep (Fst (Pair m n)) = m
+smallStep (Fst m) = Fst (smallStep m)
+smallStep (Snd (Pair m n)) = n
+smallStep (Snd m) = Snd (smallStep m)
+
+smallSteps :: Expr -> [Expr]
+smallSteps m
+  | isValue m = [m]
+  | otherwise = m : smallSteps (smallStep m)
+
+try :: Maybe a -> (a -> b) -> Maybe b -> Maybe b
+try m f z = maybe z (Just . f) m
+
+binaryCong :: (Expr -> Expr -> Expr) -> Expr -> Expr -> Maybe Expr
+binaryCong f m n = 
+  try (smallStep' m)
+    (\m' -> f m' n)
+    (try (smallStep' n)
+         (\n' -> f m n')
+         Nothing)
+
+unaryCong :: (Expr -> Expr) -> Expr -> Maybe Expr
+unaryCong f m = try (smallStep' m) f Nothing
+
+smallStep' :: Expr -> Maybe Expr
+smallStep' (Const _) = Nothing
+smallStep' (Plus (Const i) (Const j)) = Just (Const (i + j))
+smallStep' (Plus m n) = binaryCong Plus m n 
+smallStep' (Times (Const i) (Const j)) = Just (Const (i * j))
+smallStep' (Times m n) = binaryCong Times m n
+smallStep' (Negate (Const i)) = Just (Const (negate i))
+smallStep' (Negate m) = unaryCong Negate m
+smallStep' (Pair m n) = binaryCong Pair m n
+smallStep' (Fst (Pair m n)) = Just m
+smallStep' (Fst m) = unaryCong Fst m
+smallStep' (Snd (Pair m n)) = Just n
+smallStep' (Snd m) = unaryCong Snd m
+
+smallSteps' :: Expr -> [Expr]
+smallSteps' m = 
+  case smallStep' m of
+    Nothing -> [m]
+    Just m' -> m : smallSteps' m'
+
+-- examples
+e, e' :: Expr
+e = Plus (Plus (Const 3) (Const 4)) (Snd (Pair (Negate (Const 1)) (Negate (Const 2))))
+e' = Plus (Plus (Const 3) (Pair (Const 4) (Const 3))) (Snd (Pair (Negate (Const 1)) (Negate (Const 2))))
+
+
+main :: IO ()
+main = return ()
